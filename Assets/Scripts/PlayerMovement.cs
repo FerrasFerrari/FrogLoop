bc6c2b75ff6a4625651eee99297f297a17f2ca1d
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2024-10-02 08:22:14 -0300

    Dash adicionado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 7;
     public Rigidbody2D rb;
     Vector2 moveDirection;
+
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength = .3f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
     void Start()
     {
-        
+        activeMoveSpeed = moveSpeed;
     }
 
     void Update()
@@ -19,6 +27,31 @@ public class PlayerMovement : MonoBehaviour
     private void LateUpdate() {
             Inputs();
             Move();
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime; 
+        }
     }
     void Inputs(){
         float moveX = Input.GetAxisRaw("Horizontal");
@@ -27,6 +60,6 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
     void Move(){
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.velocity = new Vector2(moveDirection.x * activeMoveSpeed, moveDirection.y * activeMoveSpeed);
     }
 }
