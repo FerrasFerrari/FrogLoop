e31aba0f1a3f691398e4327e7d84df7c9d7e2887
GukoKeba <enzolegit@gmail.com>
2024-10-02 22:50:39 -0300

    Almost finished the follow mouse attack

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 7;
     public Rigidbody2D rb;
+    public Camera sceneCamera;
+    private Vector2 mousePosition;
     Vector2 moveDirection;
 
     private float activeMoveSpeed;
@@ -58,8 +61,13 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
     }
     void Move(){
         rb.velocity = new Vector2(moveDirection.x * activeMoveSpeed, moveDirection.y * activeMoveSpeed);
+    }
+    void RotateAttackPoint(Vector3 attackPoint, Vector3 axis, float angle){
+        Vector2 aimDirection = mousePosition - rb.position; 
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
     }
 }
