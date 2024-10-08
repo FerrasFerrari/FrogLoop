be2a31932ab2b6eb8b3fd98e3569d0615c43b4ae
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2024-10-08 10:24:55 -0300

    Inimigo1

---
+++ Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+    private float nextFireTime;
+    public float lineOfSite;
+    public float shootingRange;
+    public GameObject bullet;
+    public GameObject bulletParent;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceFromPlayer < lineOfSite && distanceFromPlayer > shootingRange)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (distanceFromPlayer <= lineOfSite && nextFireTime < Time.time)
+        {
+            Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
+    }
+}
