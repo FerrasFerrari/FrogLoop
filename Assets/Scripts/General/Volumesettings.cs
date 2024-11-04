90ee543ffdb5541560ec39c1f1c69aecee7305f1
Rodrigo Mamede Laurino <rlauthegentleman@gmail.com>
2024-11-04 10:39:52 -0300

    sound-options
    tô tentando fazer a unity salvar a mudança de  áudio, não terminei

--- Assets/Scripts/General/Volumesettings.cs
+++ Assets/Scripts/General/Volumesettings.cs
@@ -6,9 +6,42 @@ public class Volumesettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer MyMixer;
     [SerializeField] private Slider generalSlider;
+
+
+    private void Start()
+    {
+
+        //if (PlayerPrefs.HasKey("VolumeGeneral"))
+       //{
+       //     VolumeLoad();
+       //}
+       // else
+       //{
+            SetVolumeGeneral();
+       // }
+
+    }
+    public class AudioSettings
+    {
+        public float VolumeGeneral;
+    }
     public void SetVolumeGeneral()
     {
         float volume = generalSlider.value;
-        MyMixer.SetFloat("general", volume);
+        MyMixer.SetFloat("general", Mathf.Log10(volume) * 20);
+        //PlayerPrefs.SetFloat("VolumeGeneral", volume);
+        //PlayerPrefs.Save();
+    }
+    private void VolumeLoad()
+
+    {
+        generalSlider.value = PlayerPrefs.GetFloat("VolumeGeneral");
+        SetVolumeGeneral();
+    }
+    public AudioSettings LoadAudioSettings()
+    {
+        AudioSettings settings = new AudioSettings();
+        settings.VolumeGeneral = PlayerPrefs.GetFloat("VolumeGeneral", 1.0f); // 1.0 é o valor padrão
+        return settings;
     }
 }
