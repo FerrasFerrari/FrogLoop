e994f1c2873fd2d02292e2257808cbec884bf916
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2024-11-05 07:48:14 -0300

    Water
    Tilemap da água funcionando 100%

--- Assets/Scripts/General/Agua.cs
+++ Assets/Scripts/General/Agua.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Agua : MonoBehaviour
 {
-    BoxCollider2D _col;
+    TilemapCollider2D _col;
     public PlayerMovement PlayerMovementScript;
     // Start is called before the first frame update
     void Start()
     {
         PlayerMovementScript.Intangivel = false;
-        _col = gameObject.GetComponent<BoxCollider2D>();
+        _col = gameObject.GetComponent<TilemapCollider2D>();
     }
 
     // Update is called once per frame
