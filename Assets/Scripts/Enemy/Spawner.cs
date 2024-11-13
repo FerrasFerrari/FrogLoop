0e2689171b52f0468cdf87b5bc9af99747bc46cb
Ferdando30 <99683197+Ferdando30@users.noreply.github.com>
2024-11-13 07:49:09 -0300

    SpawnDestroyer
    quando a torre é destruída o spawn vai de base junto

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -7,6 +7,8 @@ public class Spawner : MonoBehaviour
     public GameObject EnemyPrefab;
     public float Intervalo = 2f;
     private Transform spawnPoint;
+    public Tower TowerScript;
+    public int SpawnNumber;
     // Start is called before the first frame update
     private void Start()
 
@@ -16,7 +18,26 @@ public class Spawner : MonoBehaviour
         spawnPoint = GetComponent<Transform>();
 
         InvokeRepeating("SpawnEnemy", 0f, Intervalo);
+        TowerScript.Spawn1 = true; 
+        TowerScript.Spawn2 = true;
+        TowerScript.Spawn3 = true;
 
+    }
+
+    private void Update()
+    {
+        if(SpawnNumber == 1 && TowerScript.Spawn1 == false)
+        {
+            Destroy(gameObject);
+        }
+        if(SpawnNumber == 2 && TowerScript.Spawn2 == false)
+        {
+            Destroy(gameObject);
+        }
+        if(SpawnNumber == 3 && TowerScript.Spawn3 == false)
+        {
+            Destroy(gameObject);
+        }
     }
 
 
--- Assets/Scripts/Objects/Tower.cs
+++ Assets/Scripts/Objects/Tower.cs
@@ -11,12 +11,19 @@ public class Tower : MonoBehaviour, IDamageable
     private Animator anim;
     private bool isBroken = false;
     private GateDestroier gateDestroier;
+    public bool Spawn1;
+    public bool Spawn2;
+    public bool Spawn3;
+    public int TowerSpawn;
 
 
     void Start()
     {
         anim = GetComponent<Animator>();
         gateDestroier = GameObject.FindGameObjectWithTag("Gate").GetComponent<GateDestroier>();
+        Spawn1 = true;
+        Spawn2 = true;
+        Spawn3 = true;
     }
 
     void Update()
@@ -37,5 +44,17 @@ public class Tower : MonoBehaviour, IDamageable
         anim.SetBool("isBroken", true);
         isBroken = true;
         gateDestroier.UpdateBrokenStatus(towerNumber - 1);
+        if(TowerSpawn == 1)
+        {
+            Spawn1 = false;
+        }
+        if(TowerSpawn == 2)
+        {
+            Spawn2 = false;
+        }
+        if(TowerSpawn == 3)
+        {
+            Spawn3 = false;
+        }
     }
 }
